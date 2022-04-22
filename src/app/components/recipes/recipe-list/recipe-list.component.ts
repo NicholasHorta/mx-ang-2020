47c2f7062a0e884b2e7f120d9565b8e2befe69eb
NicholasHorta <nickdeveloper.js@gmil.com>
2022-04-22 15:08:51 +0100

    Section 3 completed

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  //| When we call the Recipe model, our execution IS our constructor firing off with the information to assign
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'Simple test description',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+    new Recipe(
+      'A test recipe',
+      'Simple test description',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+    new Recipe(
+      'A test recipe',
+      'Simple test description',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+  ];
 
   constructor() { }
 
