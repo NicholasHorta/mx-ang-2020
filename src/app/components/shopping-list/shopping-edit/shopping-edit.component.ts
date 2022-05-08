40486a58b450dad8029d9986164541224166da3d
NicholasHorta <nickdeveloper.js@gmil.com>
2022-05-08 13:03:20 +0100

    Forms

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,7 +9,7 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent  {
-  @ViewChild('nameInput') nameInput: ElementRef;
+  // @ViewChild('nameInput') nameInput: ElementRef;
   // @ViewChild('amtInput') amtInput: ElementRef;
   amtValue: number = 0;
 
@@ -16,10 +17,9 @@ export class ShoppingEditComponent  {
   constructor(private shoppingListService: ShoppingListService) { }
 
 
-  onSubmit(){
-    console.log(this.amtValue)
-    const nameValue = this.nameInput.nativeElement.value;
-    const ingredient = new Ingredient(nameValue, this.amtValue);
+  onSubmit(form: NgForm){
+    console.log(form)
+    const ingredient = new Ingredient(form.value.name, form.value.amount);
     this.shoppingListService.addToList(ingredient);
   }
 
